c0560906c4c7c3e889472e1a529f3cf9ffb79863
aamiaa <9750071+aamiaa@users.noreply.github.com>
2023-09-15 16:39:41 +0200

    clarify the duplicate dm channel warning

--- src/Helpers/Consts.cs
+++ src/Helpers/Consts.cs
@@ -2,7 +2,7 @@
 {
     public class Consts
     {
-        public static readonly string DuplicateDMWarning = "You have multiple dm channels with this recipient. There is no guarantee that Discord will open the right one.";
+        public static readonly string DuplicateDMWarning = "You have multiple dm channels with this user! This is likely due to a bug which allowed opening multiple dms when messaging someone for the first time.\n\nSince Discord only saves a single dm channel as the default dm, there is no guarantee that it will open the right one.";
         public static readonly string InvalidTokenError = "Entered token is invalid or doesn't belong to the same account!";
         public static readonly string MissingTokenError = "You must enter your account token in the Settings tab to use this function.";
         public static readonly string MissingBotTokenError = "You must enter a bot token in the Settings tab to use this function.";
