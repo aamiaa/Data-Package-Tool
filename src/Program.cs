a60463720d5ff0d44a30364fd2811569bd2553a9
Inversion <neoinversion@outlook.com>
2024-03-15 22:24:31 -0700

    fix: scaling elements at high DPI

--- src/Program.cs
+++ src/Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Media;
 
+[assembly: DisableDpiAwareness]
 namespace Data_Package_Tool
 {
     static class Program
