db8028fa32a7adc6d15434d2080630d4a7b4332f
Amia <9750071+aamiaa@users.noreply.github.com>
2024-05-04 18:43:27 +0200

    fix relationship type error

--- src/Classes/Parsing/DRelationship.cs
+++ src/Classes/Parsing/DRelationship.cs
@@ -6,11 +6,25 @@ namespace Data_Package_Tool.Classes.Parsing
     {
         [JsonProperty("id")]
         public string Id { get; set; }
+
+        // Data packages from before 04.2024 have `type` set to a number,
+        // while the current ones have it set to a string.
+        // The enum resolves both automatically.
         [JsonProperty("type")]
-        public int Type { get; set; }
+        public RelationshipType Type { get; set; }
         [JsonProperty("nickname")]
         public string Nickname { get; set; }
         [JsonProperty("user")]
         public DUser User { get; set; }
+    }
+
+    public enum RelationshipType
+    {
+        MONE = 0,
+        FRIEND = 1,
+        BLOCKED = 2,
+        INCOMING_REQUEST = 3,
+        OUTGOING_REQUEST = 4,
+        IMPLICIT = 5
     }
 }
